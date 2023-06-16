5fad82235df23ac94dc8bd22a53d148b3277c113
Your Name <you@example.com>
2023-06-16 18:31:34 +0900

    first commit

---
+++ src/main/java/com/example/study_springboots/firstServlets.java
@@ -0,0 +1,18 @@
+package com.example.study_springboots;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet(urlPatterns = "/firstServlets")
+public class firstServlets extends HttpServlet{
+
+     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("성공");
+    }
+    
+    
+}
